c4a35309b72cd45f7ea03dcae31d055b28331d74
Mahongxin <1172406997@qq.com>
2022-06-24 17:51:10 +0800

    WIP: TfrMessageBox & DeleteModale

--- src/views/homePage/editLinkPage/index.vue
+++ src/views/homePage/editLinkPage/index.vue
@@ -1,12 +1,5 @@
 <template>
   <div>
-    <EditDialog
-      :visible="linkConEdit"
-      width="375px"
-      @confirm="onEdit"
-      @cancel="onCancel"
-      @delete="onCancel"
-    />
     <HasSidebarWin
       title="EDIT LINK"
       v-model="showEdit"
@@ -18,22 +11,10 @@
 
 <script setup lang="ts">
 import HasSidebarWin from '@/components/TfrDialog/HasSidebarWin.vue'
-import EditDialog from '@/components/SiteBUilderMenu/components/footerNavigation/EditDialog.vue'
 import sideArr from './setModules'
 import generalwin from '@/views/homePage/generalwin'
 import { ref } from 'vue'
 const { closeWin } = generalwin()
 
-const showPageWin = ref(false)
-const showEdit = ref(false)
-const linkConEdit = ref(true)
-
-const onCancel = () => {
-  closeWin()
-}
-const onEdit = () => {
-  showEdit.value = true
-}
+const showEdit = ref(true)
 </script>
-
-<style lang="scss" scoped></style>
