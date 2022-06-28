2a12156bab635bba4e8253a534f74e4f55aa4573
Mahongxin <1172406997@qq.com>
2022-06-28 16:28:45 +0800

    WIP: delete old Legal page

--- src/store/setBuilder/index.ts
+++ src/store/setBuilder/index.ts
@@ -2,7 +2,6 @@ import { defineStore } from 'pinia'
 import { reactive } from 'vue'
 import { sidebar } from './navigation'
 import { sidebar as footerSlidebar } from './footerNavigation'
-import { sidebar as legalSlidebar } from './legalNavigation'
 import { PageSchema, ComponentsSchema } from '@/views/homePage/type/index'
 import { SITE_PAGETEMPLATE } from '@/views/homePage/config/templateMap'
 interface Basic {
@@ -11,7 +10,7 @@ interface Basic {
 }
 
 export const setBuilder = defineStore('setBuilder', () => {
-  const sideState = { ...sidebar(), ...footerSlidebar(), ...legalSlidebar() }
+  const sideState = { ...sidebar(), ...footerSlidebar() }
 
   const basic = reactive<Basic>({
     platformState: 'pc',
