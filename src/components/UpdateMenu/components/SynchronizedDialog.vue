1c59424ac0473d5dda14b85372b09c613e1e1e7a
Mahongxin <1172406997@qq.com>
2022-08-09 16:25:12 +0800

    WIP：add update some model in page

--- src/components/UpdateMenu/components/SynchronizedDialog.vue
+++ src/components/UpdateMenu/components/SynchronizedDialog.vue
@@ -7,7 +7,7 @@
     :header-less="false"
     :append-to-body="true"
   >
-    <template #header> CHOOSE CONTENT </template>
+    <template #header> SYNCHRONIZED </template>
     <div class="animate-svg">
        <lottie-player
           src="https://assets1.lottiefiles.com/packages/lf20_zw5fzhuo.json"
@@ -16,7 +16,7 @@
           autoplay
         ></lottie-player>
     </div>
-    <div class="text-tip">Check pending or individual page(s) to publish.</div>
+    <div class="text-tip">Contents in sync. There's nothing to publish.</div>
     <template #footer>
       <TfrButton type="normal" :board="false" @click="onOK">OK</TfrButton>
     </template>
